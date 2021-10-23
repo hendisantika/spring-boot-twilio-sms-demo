e8a5b5d97bcd7e67e401bf4a5d45ce41fb9399ae
Hendi Santika <hendisantika@gmail.com>
2021-10-23 12:59:54 +0700

    #9 - Add sendSms in TwilioSmsSender

--- src/main/java/com/hendisantika/config/TwilioSmsSender.java
+++ src/main/java/com/hendisantika/config/TwilioSmsSender.java
@@ -1,6 +1,10 @@
 package com.hendisantika.config;
 
+import com.hendisantika.dto.SmsRequest;
 import com.hendisantika.service.SmsSender;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
+import com.twilio.type.PhoneNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +31,25 @@ public class TwilioSmsSender implements SmsSender {
         this.twilioConfiguration = twilioConfiguration;
     }
 
+    @Override
+    public void sendSms(SmsRequest smsRequest) {
+        if (isPhoneNumberValid(smsRequest.getPhoneNumber())) {
+            PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
+            PhoneNumber from = new PhoneNumber(twilioConfiguration.getTrialNumber());
+            String message = smsRequest.getMessage();
+            MessageCreator creator = Message.creator(to, from, message);
+            creator.create();
+            LOGGER.info("Send sms {}", smsRequest);
+        } else {
+            throw new IllegalArgumentException(
+                    "Phone number [" + smsRequest.getPhoneNumber() + "] is not a valid number"
+            );
+        }
+
+    }
+
+    private boolean isPhoneNumberValid(String phoneNumber) {
+        // TODO: Implement phone number validator
+        return true;
+    }
 }
