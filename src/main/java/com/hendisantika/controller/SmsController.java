52d656b9d75d535718989f93a8579bf81f68b998
Hendi Santika <hendisantika@gmail.com>
2021-10-23 13:06:57 +0700

    #12 - Add sendSms in SmsController

--- src/main/java/com/hendisantika/controller/SmsController.java
+++ src/main/java/com/hendisantika/controller/SmsController.java
@@ -1,10 +1,15 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.SmsRequest;
 import com.hendisantika.service.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,5 +30,10 @@ public class SmsController {
     @Autowired
     public SmsController(SmsService smsService) {
         this.smsService = smsService;
+    }
+
+    @PostMapping
+    public void sendSms(@Valid @RequestBody SmsRequest smsRequest) {
+        smsService.sendSms(smsRequest);
     }
 }
